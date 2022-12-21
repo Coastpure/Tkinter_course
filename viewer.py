3d48e91d458b1d12deac190db84868ed1d9add24
Coastpure <njorogesimon465@gmail.com>
2022-12-21 23:46:55 +0300

    Image viewer complete

--- viewer.py
+++ viewer.py
@@ -11,21 +11,68 @@ my_img3 = ImageTk.PhotoImage(Image.open("images/bag1.jpg"))
 my_img4 = ImageTk.PhotoImage(Image.open("images/city2.jpg"))
 my_img5 = ImageTk.PhotoImage(Image.open("images/headphone1.jpg"))
 
+# we created all our images into a python list
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 
 my_label = Label(image=my_img1)
-
 # below image, we're going to have 3 buttons, next back and exit'
 my_label.grid(row=0, column=0, columnspan=3)
 
+
+def forward(image_number):
+    global my_label
+    global button_forward
+    global button_back
+
+    # what is happening in this function
+    # we create a counter, to reference the next item in that list, forget the previous one using my_label.grid_forget()
+    # and everytime we click the buttons we just need to update the buttons, which is why we created the globals,
+    # so that the updated buttons will work outside the function
+
+    # make the image disappear after we click next
+    my_label.grid_forget()
+
+    my_label = Label(image=image_list[image_number - 1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_number + 1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number - 1))
+
+    if image_number == 5:
+        button_forward = Button(root, text=">>", state=DISABLED)
+
+    # display buttons on screen
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
+
+
+def back(image_number):
+    global my_label
+    global button_forward
+    global button_back
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number - 1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_number + 1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number - 1))
+
+    if image_number ==1:
+        button_back = Button(root, text="<<", state=DISABLED)
+
+    # display buttons on screen
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
+
+
 # buttons
-button_back = Button(root, text="<<")
+button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_exit = Button(root, text="Exit Program", command=root.quit)
-button_forward = Button(root, text=">>")
+button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 # button positioning
-button_back.grid(row=1,column=0)
-button_exit.grid(row=1,column=1)
-button_forward.grid(row=1,column=2)
+button_back.grid(row=1, column=0)
+button_exit.grid(row=1, column=1)
+button_forward.grid(row=1, column=2)
 
 root.mainloop()
+
